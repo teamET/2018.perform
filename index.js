8052ba49e040e82c68b9b037239a52fee7f08bb9
Akari000 <app.sub.k@gmail.com>
2018-07-05 19:55:39 +0900

    add top page

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 console.log("hello sakakendo");
+var express=require("express");
+var app=express();
+var ejs=require("html");
+/*
+var ejs=require('ejs');
+app.set('view engine', 'ejs');
+*/
+app.use(function (req, res, next) {
+	console.log('Request URL:', req.originalUrl);
+	console.log('Time:', Date.now());
+	next();
+});
+
+
+app.get('/shop',function (req, res) {
+	res.render('shop', { title: '模擬店を探す'});
+});
+app.get('/ivent',function (req, res) {
+	res.render('ivent', { title: 'イベントタイムスケジュール'});
+});
+app.get('/',function (req, res) {
+	res.render('top');
+});
