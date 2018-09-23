b1cc44d3069be275b4cde6ebf371e22380ccb0d8
sakakendo <sakakendo0321@gmail.com>
2018-09-23 20:27:06 +0900

    fix bot message

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 source ~/.bashrc
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot. `$(whoami)@$(hostname -I|cut -f1 -d' ')`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot. `$(whoami)@$(hostname -I|cut -f1 -d' ')`\n\`$(pwd)\`\n\`\`\`$(ls -al)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
 
 # hexo
 #cd static && hexo generate && ln -s public /var/www/public
