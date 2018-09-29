9ffed755be4a7865870b6344a545f7bfc0d3869e
sun-yryr <taittide@gmail.com>
2018-09-30 03:05:56 +0900

    add - bodyparser

--- api-server/app.js
+++ api-server/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var fluentLogger = require('fluent-logger').configure('tag_prefix', {
   host: 'localhost',
   port: 24224,
@@ -22,7 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 //app.use(app.router);
 //app.use(app.errorHandler());
