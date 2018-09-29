48704593ba6722255b0e182f856cb602792d966b
sun-yryr <taittide@gmail.com>
2018-09-30 02:53:41 +0900

    line認証-接続

--- api-server/app.js
+++ api-server/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
 
 //app.use(app.router);
 //app.use(app.errorHandler());
--- api-server/routes/linebot.js
+++ api-server/routes/linebot.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
+var request = require('request');
+
+
+/* 環境変数 */
+const channelSecret = "cf2ce152898f4d3465d1e5f2d3dd46e8";
+const accessToken = "0dsdbxxcI/wkh1vXXjh+GxNqS1bFbVRYH4v0zWiKURT9qfK/Gn9huqMLB2/LyLzCf8q56GSJ809ankSwwf+35nE0hXZCizbw5+TLTZ2oH2l/4oHhhuHkEM8tK6SCDPcQREcaofZqeRMW5jbTbo8WigdB04t89/1O/w1cDnyilFU=";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({"name": "sun-yryr"});
+    var responce = "";
+    /* LINE認証 */
+    const body = req.body; // Request body string
+    const signature = crypto
+        .createHmac('SHA256', channelSecret)
+        .update(JSON.stringify(body)).digest('base64');
+    if (req.header("x-line-signature") == signature) {
+        //認証成功
+        res.status(200);
+        //以下デバッグ用
+        console.log("line_OK");
+        const event = body.events[0];
+        if (event.type == "message"){
+            request.post({
+                url: url_reply,
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                },
+                json: {
+                    "replyToken": event.replyToken,
+                    "messages": [
+                        {
+                            "type": "text",
+                            "text": event.message.text
+                        }
+                    ]
+                }
+            }, function (error, response, body){
+                context.log(body);
+            });
+        }
+    } else {
+        //認証失敗
+        console.log("line_NG");
+        res.status(403);
+    }
+    console.log("end");
+    res.send(responce);
 });
 
 module.exports = router;
