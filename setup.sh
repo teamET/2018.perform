87039b103adf2a44189498196ec3fa3a9510e424
sakakendo0321 <sakakendo0321@gmail.com>
2018-09-29 13:36:26 +0900

    デプロイの自動化 (#154)
    * add development and production server settings
    
    * fix key-scan
    
    * fix directory
    
    * delete nohup
    
    * make rsync be silent
    
    * load nvm
    
    * fix data directory
    
    * fix cd
    
    * fix cd
    
    * change user kfes to root
    
    * fix sudo to root
    
    * replacement (bash -> source)
    
    * test for successness of circleci
    
    * gegt version of command
    
    * add ssh config read
    
    * fix source command
    
    * add hexo commands in circleci
    
    * add profile file
    
    * public to static
    
    * add slack message
    
    * add log for slack
    
    * del slack mes
    
    * fix syntax
    
    * show public files
    
    * fix rsync files
    
    * fix cd
    
    * fix hexo
    
    * fix hexo installation command
    
    * change hexo installation option -g to ---save
    
    * copy public files
    
    * make shortern log
    
    * fix copy directory
    
    * add test.json
    
    * add /var/www/public/data
    
    * Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
-source ~/.bashrc
-
-cd 2018.perform
-curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot.\n http://$(hostname -I|cut -f1 -d' ')\n \`$(whoami)@$(hostname -I|cut -f1 -d' ')\`\n\`$(pwd)\`\n\`\`\`$(ls -al)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+source /root/.bash_profile
+hexo --version
+npm --version
+node --version
 
 # hexo
-#cd static && hexo generate && ln -s public /var/www/public&&cd -
+cd /root/2018.perform/static
+hexo --version
+npm install --save hexo-cli
+hexo generate
+cp -rf ./public/* /var/www/public
+mkdir /var/www/public/data
+echo "{'test':'test'}" > /var/www/public/data/test.json
 
 # bot
-#cd bot&& npm install && ln -s files /var/www/api/files && forever bot.js
+cd /root/2018.perform/bot
+npm install
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot.\n http://$(hostname -I|cut -f1 -d' ')\n \`$(whoami)@$(hostname -I|cut -f1 -d' ')\`\n\`$(pwd)\`\n\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"\`\`\`$(cd /var/www/public ;find .)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+
