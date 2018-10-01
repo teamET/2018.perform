102c6ef87fabe377a845fd0e15dd66d2089a2e7d
sun-yryr <taittide@gmail.com>
2018-10-01 14:26:48 +0900

    build_msgの変更（複数）

--- api-server/routes/linebot.js
+++ api-server/routes/linebot.js
@@ -3,11 +3,23 @@ var router = express.Router();
 const crypto = require('crypto');
 var request = require('request');
 var moment = require('moment');
+const dialogflow = require("dialogflow");
 var connection = require('./mysqlConnection');
 
 /* 環境変数 */
 const channelSecret = "cf2ce152898f4d3465d1e5f2d3dd46e8";
 const accessToken = "0dsdbxxcI/wkh1vXXjh+GxNqS1bFbVRYH4v0zWiKURT9qfK/Gn9huqMLB2/LyLzCf8q56GSJ809ankSwwf+35nE0hXZCizbw5+TLTZ2oH2l/4oHhhuHkEM8tK6SCDPcQREcaofZqeRMW5jbTbo8WigdB04t89/1O/w1cDnyilFU=";
+const GOOGLE_PROJECT_ID = "kufes-2018";
+const GOOGLE_CLIENT_EMAIL = "dialogflow-qulnmn@kufes-2018.iam.gserviceaccount.com";
+const GOOGLE_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC5NHF7eqbHOyIU\nJIhQJ81rO/C+lxIFMY9VCugm04tUwgvEakK48TMzTNxmFB89MN8jvkCPFHSMuW5A\nNTQNwai2y6kASQVQmHtRBTYaN316NulGcUkhg9ORgSOAuB/VUHqDyHgDUjRFknj4\n0efJpw9ECkxzXP2e0rz/T72By9JxSxkoRq6x4N5k1v4Jr3+J8rHG8D4pZRxZ+olc\nhXdJrPv8QZ56o+TDBywYZHN40zStH+r8QocTPpO+tloegNmjyGZUkAQN8qAMNhyi\niTr6H8yHXrFhihDhtKlR3YHC0b9ON9euUPblU8hdqoGC0ZP+cPrry7BAFyumWJ1v\nR5GgmRTxAgMBAAECggEACmZHeQeKFeXcJAVpIhcaEctr2UfiWyhNRBk4r/Vw/XFn\n979/v6LLnTXTqme22VjFLtygA2jCoqRG7JQQODWHo7wL4Vg4VC47vnDseMxk31f2\nAV6bHKaqWqFFvRSZtQCv4HaBRq53APEYmeAvg8M9uDRb1p6CH0j2E+AGZCNtza6x\nXEFU5va8dc6dcIZcRU7MgpWleZM54MLvffImAosNRSiEtbPqg/YUN/39bybmD9l/\noQBpyfd97oFi4VYsQkthfJ52WZg/RlWG44s/WWe/wL8WWftiIRWylLYwfJjPVtuZ\nRslJm1x6JuU1ATM69RNzbk7HIsvQopZUEg5Kg5p4AQKBgQDmwc0LBtocf3N6QkYA\ntIqNLEi3gkveUFgOjl8nPpT1t8JfW7WzBg3Dfox4V8y0xuqqcx1nyXLj87Hs51t0\nyVPRq5XgNC8gSCaPitRWRu2KIEHy7mQVIXwtSWykekd+KpTaGmlxHoOx67TW6mS7\nJHh8oC81bmSUXZiehRAiVpaXyQKBgQDNdvvI7zm4Hn8dcFSSDoPXtTepNhNv2OHn\n0nza2O7a2CEQfypIkGlBx1MmvIzQrpYfhup+SbTgWYjIS6oDbfVuQpSlhCgrRfPr\nEf4nKK9lbd8IZI7hSJScsz1NgcWHSLghB3uHrecT/AWIwiXhLY9GhVX6taqPmqF6\n/hZIM2b36QKBgQCL+JhwjNp2mNSqH1MpEFpOocMGFTICCwgu5CtRucNPfQSZJR8F\nFbH5mGSKhu9z5IjplWQL1YUsQmD1y6yNHaYLM6J42g6P7VP+k/6SyvlBZKm8OuBY\ndPRG4BfXeRiurhOWbJjy9ch6fvg1uP4bCldPeTbJmUnHOTLfTOVpfs5gCQKBgFu8\nbSzIyt+PFjhBqDDSNEGCUsjFMSZ2El8cFszroLGrYA/qhymA+M36vgCEnOarnLGU\n3mvmYtDsiOrNBaqkVLmXFqFUAU9Y21AwZ2Z6ft1tkfBAXZ7udhQE3zEU6Om/KR7u\nJVRt68d7dckazijc26Sj8cCPjgiyBLlSawZvlODJAoGBALYo35VcRb5FIqF/Kmuj\nW4/pj6MkXK2OPhmPYCTJVkDUJo6EAZhX+/6UlJ2bkhomLujH/YeXwAF9Z/ABFLZz\nzFeR7r6Bh+dhnFq8/63SKwV9mfCpsREMpLarNajITKqu4szANjuuiy3iZyKe2qeD\n9hv5A9oLL+Mq3rLhnCvq7SF4\n-----END PRIVATE KEY-----\n";
+
+const session_client = new dialogflow.SessionsClient({
+    project_id: GOOGLE_PROJECT_ID,
+    credentials: {
+        client_email: GOOGLE_CLIENT_EMAIL,
+        private_key: GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    }
+});
 
 /* LINE MessagingAPI URL */
 //URL POST
@@ -35,23 +47,46 @@ function Build_responce(url, res_body) {
 }
 
 /* メッセージの作成 */
-function Build_msg_text(Token, message) {
+function Build_msg_text(Token, message1, message2, message3, message4, message5) {
     return new Promise(function(resolve, reject) {
         var tmp = {
             "replyToken": Token,
-            "messages": [{
-                "type": "text",
-                "text": message
-            }]
+            "messages": []
         };
+        if (message1) {
+            tmp.messages.push(message1);
+            if (message2) {
+                tmp.messages.push(message2);
+                if (message3) {
+                    tmp.messages.push(message3);
+                    if (message4) {
+                        tmp.messages.push(message4);
+                        if (message5) {
+                            tmp.messages.push(message5);
+                        }
+                    }
+                }
+            }
+        }
         resolve(tmp);
+    });
+}
+
+/* テンプレートメッセージの作成 */
+function Build_msg_template(Token) {
+    return new Promise(function(resolve, reject) {
     });
 }
 
 /* Type - message */
 async function type_message(event) {
+    // Dialogflowへの接続
+    var msg = {
+        "type": "text",
+        "text": event.message.text
+    };
     var tmp = await Build_responce(urlp_reply, await Build_msg_text(
-        event.replyToken, event.message.text
+        event.replyToken, msg
     ));
     request.post(tmp, function(error, responce, body) {
         console.log(body);
@@ -64,9 +99,12 @@ async function type_follow(event) {
     request.post(await Build_responce(rich_url), function(error, responce, body) {
         console.log(body);
     });
-    var message = "東京高専文化祭BOTを友達登録してくれてありがとう！";
+    var msg = {
+        "type": "text",
+        "text": "東京高専文化祭BOTを友達登録してくれてありがとう！"
+    };
     var tmp = await Build_responce(urlp_reply, await Build_msg_text(
-        event.replyToken, message
+        event.replyToken, msg
     ));
     request.post(tmp, function(error, responce, body) {
         console.log(body);
@@ -78,8 +116,12 @@ async function addUser(event, usertype) {
     request.delete(await Build_responce(rich_url), function(error, responce, body) {
         console.log(body);
     });
+    var msg = {
+        "type": "text",
+        "text": usertype + "と認証しました"
+    };
     var tmp = await Build_responce(urlp_reply, await Build_msg_text(
-        event.replyToken, usertype + "と認証しました"
+        event.replyToken, msg
     ));
     request.post(tmp, function(error, responce, body) {
         console.log(body);
@@ -106,8 +148,12 @@ function removeUser(event) {
 
 /* Type - Beacon */
 async function type_beacon(event) {
+    var msg = {
+        "type": "text",
+        "text": "ビーコン範囲に入りました"
+    };
     var tmp = await Build_responce(urlp_reply, await Build_msg_text(
-        event.replyToken, "ビーコン範囲に入りました"
+        event.replyToken, msg
     ));
     request.post(tmp, function(error, responce, body) {
         console.log(body);
