3d3a92ad53eec4d3b609ebc09d8a50c7ac84794e
sakakendo <sakakendo0321@gmail.com>
2018-10-02 12:54:26 +0900

    add some func

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 source /root/.bash_profile
+
+function start_forever(){
+    echo start process $1 , $2
+    cd $1
+    npm install
+    forever restart $2 --minUptime 1000 --spinSleepTime 1000 || \
+        forever restart $2 --minUptime 1000 --spinSleepTime 1000 && forever list)
+}
+
+function mes(){
+    echo $1
+    curl -X POST --data-urlencode \
+    "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\` $1 \`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+}
+
 hexo --version
 npm --version
 node --version
@@ -10,28 +25,14 @@ hexo generate --force --bail
 cp -rf ./public/* /var/www/public
 echo "{'test':'test'}" > /var/www/public/data/test.json  #/var/www/public/data should be directory
 
-# bot
-cd /root/2018.perform/bot
-npm install
-if  forever list |grep bot.js >/dev/null; then
-    forever restart --minUptime 1000 --spinSleepTime 1000 /root/2018.perform/bot/bot.js
-else
-    forever restart --minUptime 1000 --spinSleepTime 1000 /root/2018.perform/bot/bot.js
-fi
-
-#api-server
-cd /root/2018.perform/api-server
-npm install
-if forever list |grep api-server < /dev/null ; then
-    forever restart --minUptime 1000 --spinSleepTime 1000 /root/2018.perform/api-server/bin/www
-else
-    forever start --minUptime 1000 --spinSleepTime 1000 /root/2018.perform/api-server/bin/www
-fi
 
 
+start_forever /root/2018.perform/bot/ bot.js
+start_forever /root/2018.perform/api-server/ bin/www
 
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot.\n http://$(hostname -I|cut -f1 -d' ')\n \`$(whoami)@$(hostname -I|cut -f1 -d' ')\`\n\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\`$(cd /var/www/public ;find .|sort)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
 
+mes "hello slack func"
