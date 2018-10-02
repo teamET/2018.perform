7b86ced20bf895d3b5dc345c6f7def0b5f8cba7b
sakakendo <sakakendo0321@gmail.com>
2018-10-02 22:24:15 +0900

    add find and echo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 source $HOME/.bash_profile
+echo $WEBHOOK_URL
 
 function start_forever(){
     echo start process $1 , $2
@@ -10,8 +11,8 @@ function start_forever(){
 
 function mes(){
     echo $1
-    curl -X POST --data-urlencode \
-    "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\` $1 \`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+    curl -X POST $WEBHOOK_URL --data-urlencode \
+    "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\` $@ \`\`\`\", \"icon_emoji\": \":sunglasses:\"}" 
 }
 
 hexo --version
@@ -23,6 +24,8 @@ cd $HOME/2018.perform/static
 npm install --save hexo-cli
 hexo generate --force --bail
 \cp -rf ./public/* /var/www/public
+mes "* ./public" $(find ./public|sort)
+mes "* /var/www/public" $(find /var/www/public|sort)
 echo "{'test':'test'}" > /var/www/public/data/test.json  #/var/www/public/data should be directory
 
 
@@ -34,6 +37,6 @@ forever list
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#bot\", \"username\": \"webhookbot\", \"text\": \"This is posted to #bot and comes from a bot named webhookbot.\n http://$(curl ifconfig.io)\n \`$(whoami)@$(curl ifconfig.io)\`\n\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\`$(cd /var/www/public ;find .|sort)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
+#curl -X POST --data-urlencode "payload={\"channel\": \"#server_log\", \"username\": \"webhookbot\", \"text\": \"\`\`\`$(cd /var/www/public ;find .|sort)\`\`\`\", \"icon_emoji\": \":sunglasses:\"}" $WEBHOOK_URL
 
 mes "hello slack func"
