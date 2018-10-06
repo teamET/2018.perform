92ba379cba63027564cc7e4b1f8f05bff3bf478c
sho0126hiro <sho0126hiro@yahoo.co.jp>
2018-10-06 21:39:05 +0900

    path

--- api-server/routes/linebot.js
+++ api-server/routes/linebot.js
@@ -224,7 +224,7 @@ function msg_imagemap(usage,data){
     if(usage == "map"){
         var msg = {
             "type": "imagemap",
-            "baseUrl": "./routes/linebot_mapImg/map-InOutChoice.png",
+            "baseUrl": "./routes/linebot_mapImg/map-InOutChoice/1040",
             "altText": "This is an imagemap",
             "baseSize": {
                 "height": 585,
